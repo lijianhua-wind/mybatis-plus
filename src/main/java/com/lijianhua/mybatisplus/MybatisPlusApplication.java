de0ba6b38e9db476262ccad514514f500e492dd1
wind <1976288368@qq.com>
2022-10-27 09:50:32 +0800

    勉强配置好

--- src/main/java/com/lijianhua/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/com/lijianhua/mybatisplus/MybatisPlusApplication.java
@@ -1,13 +1,15 @@
 package com.lijianhua.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lijianhua.mybatisplus.mapper]")
 public class MybatisPlusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybatisPlusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/lijianhua/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.lijianhua.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lijianhua.mybatisplus.domain.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}
